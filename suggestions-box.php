8fee4233845bfdd96a14e061f50d6830817a2804
gmaph001 <gmaph001@gmail.com>
2024-10-23 14:34:01 +0300

    deploy commit

--- suggestions-box.php
+++ suggestions-box.php
@@ -1,43 +1,64 @@
-<?php
-    require_once "config.php";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Suggestion | SUCCESS</title>
+     <link rel="stylesheet" type="text/css" href="register.css">
+     <link rel="icon" type="image/x-icon" href="media/images/srss-logo.jfif">
+</head>
+<body>
+    <div class="register">
+        <?php
+            error_reporting(E_ALL);
 
-    $uname = $_GET['uname'];
+            require_once "config.php";
 
-    $query = "SELECT * FROM students";
+            $uname = $_GET['uname'];
 
-    $result = mysqli_query($db, $query);
+            $query = "SELECT * FROM students";
+
+            $result = mysqli_query($db, $query);
 
 
-    if(isset($_POST['tuma'])){
-        $privacy = $_POST['seen'];
-        $suggestions = $_POST['suggestions'];
+            if(isset($_POST['tuma'])){
+                $privacy = $_POST['seen'];
+                $suggestions = $_POST['suggestions'];
 
-        if($result){
-            for($i=0; $i<mysqli_num_rows($result); $i++){
-                $row = mysqli_fetch_array($result);
-                if($row['userkey'] === $uname){
-                    $firstname = $row['firstname'];
-                    $secondname = $row['secondname'];
-                    $lastname = $row['lastname'];
-                    $username = $row['username'];                   
+                if($result){
+                    for($i=0; $i<mysqli_num_rows($result); $i++){
+                        $row = mysqli_fetch_array($result);
+                        if($row['userkey'] === $uname){
+                            $firstname = $row['firstname'];
+                            $secondname = $row['secondname'];
+                            $lastname = $row['lastname'];
+                            $username = $row['username']; 
+                            $user_ID = $row['userkey'];
+                        }
+                    }
+                }
+
+                if($privacy == NULL){
+                    $privacy = "private";
+                }
+                else{
+                    $privacy = "public";
                 }
             }
-        }
 
-        if($privacy == NULL){
-            $privacy = "private";
-        }
-        else{
-            $privacy = "public";
-        }
+            $sql = "INSERT INTO suggestions (user_ID, firstname, secondname, lastname, username, suggestions, privacy) VALUES (?,?,?,?,?,?,?)";
+            $stmtinsert = $db->prepare($sql);
+            $result2 = $stmtinsert->execute([$user_ID, $firstname, $secondname, $lastname, $username, $suggestions, $privacy]);
 
-        $query2 = "INSERT INTO suggestions(user_ID, firstname, secondname, lastname, username, suggestions, privacy) VALUES('$uname', '$firstname', '$secondname', '$lastname', '$username', '$suggestions', '$privacy')";
-        $result2 = mysqli_query($db, $query2);
-
-        if($result2){
-            echo "Thank You For Your Suggestions!";
-        }
-        else{
-            echo "Sorry! Your suggestions are not submitted, due to the problem at our end!";
-        }
-    }
+            if ($result2) {
+                echo "<p>Thank You For Your Suggestions!</p><br>";
+                echo "<p>You can now continue posting more suggestions! <a href=suggest.php?uname=$uname>Continue</a></p>";
+            }
+            else{
+                echo "<p>Sorry! Suggestion failed to be submitted due to an error at our end!</p><br>";
+                echo "<p>Please contact the developer! <a href=home.php?uname=$uname&&#footer>Continue</a></p>";
+            }
+        ?>
+    </div>
+</body>
+</html>
