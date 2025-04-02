6c6f27927b36b8cbb2d193902f57160c754274d3
gmaph001 <gmaph001@gmail.com>
2025-04-02 14:33:20 +0300

    developer privilege, database change and more security

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 <head>
      <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Login-page</title>
+     <meta name="description" content="Welcome to Najifunza System, a system intergrated for sharing academic materials">
+     <title>NAJIFUNZA</title>
      <link rel="preconnect" href="https://fonts.googleapis.com">
      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
      <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
